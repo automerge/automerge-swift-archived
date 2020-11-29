3710455df15ba4eabecc165df25e4b2ab3d352b2
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2020-11-29 13:23:31 +0100

    removes unused JS implementations

--- Source/Context.swift
+++ Source/Context.swift
@@ -134,27 +134,6 @@ final class Context {
         }
     }
 
-    //    setValue(objectId, key, value, insert) {
-    //      if (!objectId) {
-    //        throw new RangeError('setValue needs an objectId')
-    //      }
-    //      if (key === '') {
-    //        throw new RangeError('The key of a map entry must not be an empty string')
-    //      }
-    //
-    //      if (isObject(value) && !(value instanceof Date) && !(value instanceof Counter)) {
-    //        // Nested object (map, list, text, or table)
-    //        return this.createNestedObjects(objectId, key, value, insert)
-    //      } else {
-    //        // Date or counter object, or primitive value (number, string, boolean, or null)
-    //        const description = this.getValueDescription(value)
-    //        const operation = Object.assign({action: 'set', obj: objectId, key}, description)
-    //        if (insert) operation.insert = true
-    //        this.addOp(operation)
-    //        return description
-    //      }
-    //    }
-
     /**
      * Recursively creates Automerge versions of all the objects and nested objects in `value`,
      * constructing a patch and operations that describe the object tree. The new object is
@@ -225,18 +204,6 @@ final class Context {
             subPatch.props?[.index(index + offset)] = [actorId.actorId: valuePatch]
         })
     }
-    //    insertListItems(subpatch, index, values, newObject) {
-    //      const list = newObject ? [] : this.getObject(subpatch.objectId)
-    //      if (index < 0 || index > list.length) {
-    //        throw new RangeError(`List index ${index} is out of bounds for list of length ${list.length}`)
-    //      }
-    //
-    //      for (let offset = 0; offset < values.length; offset++) {
-    //        const valuePatch = this.setValue(subpatch.objectId, index + offset, values[offset], true)
-    //        subpatch.edits.push({action: 'insert', index: index + offset})
-    //        subpatch.props[index + offset] = {[this.actorId]: valuePatch}
-    //      }
-    //    }
 
     /**
      * Updates the list object at path `path`, deleting `deletions` list elements starting from
@@ -270,30 +237,6 @@ final class Context {
         updated[ROOT_ID] = cache[ROOT_ID]
 
     }
-    //    splice(path, start, deletions, insertions) {
-    //      const objectId = path.length === 0 ? ROOT_ID : path[path.length - 1].objectId
-    //      let list = this.getObject(objectId)
-    //      if (start < 0 || deletions < 0 || start > list.length - deletions) {
-    //        throw new RangeError()
-    //      }
-    //      if (deletions === 0 && insertions.length === 0) return
-    //
-    //      let patch = {diffs: {objectId: ROOT_ID, type: 'map'}}
-    //      let subpatch = this.getSubpatch(patch, path)
-    //      if (!subpatch.edits) subpatch.edits = []
-    //
-    //      if (deletions > 0) {
-    //        for (let i = 0; i < deletions; i++) {
-    //          this.addOp({action: 'del', obj: objectId, key: start})
-    //          subpatch.edits.push({action: 'remove', index: start})
-    //        }
-    //      }
-    //
-    //      if (insertions.length > 0) {
-    //        this.insertListItems(subpatch, start, insertions, false)
-    //      }
-    //      this.applyPatch(patch.diffs, this.cache[ROOT_ID], this.updated)
-    //    }
 
     /**
      * Updates the map object at path `path`, setting the property with name
@@ -317,7 +260,6 @@ final class Context {
      * Updates the map object at path `path`, setting the property with name
      * `key` to `value`.
      */
-
     func setMapKey<T: Equatable>(path: [KeyPathElement], key: String, value: T) {
         let objectId = path.isEmpty ? ROOT_ID : path[path.count - 1].objectId
         let object = getObject(objectId: objectId)
@@ -336,26 +278,6 @@ final class Context {
         }
 
     }
-    //    setMapKey(path, key, value) {
-    //      if (typeof key !== 'string') {
-    //        throw new RangeError(`The key of a map entry must be a string, not ${typeof key}`)
-    //      }
-    //
-    //      const objectId = path.length === 0 ? ROOT_ID : path[path.length - 1].objectId
-    //      const object = this.getObject(objectId)
-    //      if (object[key] instanceof Counter) {
-    //        throw new RangeError('Cannot overwrite a Counter object; use .increment() or .decrement() to change its value.')
-    //      }
-    //
-    //      // If the assigned field value is the same as the existing value, and
-    //      // the assignment does not resolve a conflict, do nothing
-    //      if (object[key] !== value || Object.keys(object[CONFLICTS][key] || {}).length > 1 || value === undefined) {
-    //        this.applyAtPath(path, subpatch => {
-    //          const valuePatch = this.setValue(objectId, key, value, false)
-    //          subpatch.props[key] = {[this.actorId]: valuePatch}
-    //        })
-    //      }
-    //    }
 
 
     /**
@@ -383,33 +305,6 @@ final class Context {
             fatalError()
         }
     }
-    //    getValueDescription(value) {
-    //      if (!['object', 'boolean', 'number', 'string'].includes(typeof value)) {
-    //        throw new TypeError(`Unsupported type of value: ${typeof value}`)
-    //      }
-    //
-    //      if (isObject(value)) {
-    //        if (value instanceof Date) {
-    //          // Date object, represented as milliseconds since epoch
-    //          return {value: value.getTime(), datatype: 'timestamp'}
-    //
-    //        } else if (value instanceof Counter) {
-    //          // Counter object
-    //          return {value: value.value, datatype: 'counter'}
-    //
-    //        } else {
-    //          // Nested object (map, list, text, or table)
-    //          const objectId = value[OBJECT_ID]
-    //          if (!objectId) {
-    //            throw new RangeError(`Object ${JSON.stringify(value)} has no objectId`)
-    //          }
-    //          return {objectId, type: this.getObjectType(objectId)}
-    //        }
-    //      } else {
-    //        // Primitive value (number, string, boolean, or null)
-    //        return {value}
-    //      }
-    //    }
 
     /**
      * Returns a string that is either 'map', 'table', 'list', or 'text', indicating
@@ -428,14 +323,6 @@ final class Context {
             return .map
         }
     }
-    //    getObjectType(objectId) {
-    //      if (objectId === ROOT_ID) return 'map'
-    //      const object = this.getObject(objectId)
-    //      if (object instanceof Text) return 'text'
-    //      if (object instanceof Table) return 'table'
-    //      if (Array.isArray(object)) return 'list'
-    //      return 'map'
-    //    }
 
     /**
      * Returns an object (not proxied) from the cache or updated set, as appropriate.
@@ -458,12 +345,6 @@ final class Context {
         }
         return object
     }
-
-    //    getObject(objectId) {
-    //      const object = this.updated[objectId] || this.cache[objectId]
-    //      if (!object) throw new RangeError(``)
-    //      return object
-    //    }
 
     /**
      * Constructs a new patch, calls `callback` with the subpatch at the location `path`,
@@ -517,21 +398,16 @@ final class Context {
      */
     func getPropertyValue(object: [String: Any], key: Key, opId: String) -> [String: Any] {
         if object[TABLE_VALUES] != nil {
-            if case .string(let stringKey) = key, let entries = object[TABLE_VALUES] as? [String: [String: Any]], let value = entries[stringKey] {
-                return value
+            guard case .string(let stringKey) = key,
+               let entries = object[TABLE_VALUES] as? [String: [String: Any]],
+               let value = entries[stringKey] else {
+                fatalError()
             }
-            fatalError()
+            return value
         } else {
             return ((object[CONFLICTS] as! [Key: Any])[key] as! [String: Any])[opId] as! [String: Any]
         }
     }
-    //    getPropertyValue(object, key, opId) {
-    //      if (object instanceof Table) {
-    //        return object.byId(key)
-    //      } else {
-    //        return object[CONFLICTS][key][opId]
-    //      }
-    //    }
 
     /**
      * Builds the values structure describing a single property in a patch. Finds all the values of
@@ -559,27 +435,6 @@ final class Context {
             return values
         }
     }
-    //    getValuesDescriptions(path, object, key) {
-    //      if (object instanceof Table) {
-    //        // Table objects don't have conflicts, since rows are identified by their unique objectId
-    //        const value = object.byId(key)
-    //        if (value) {
-    //          return {[key]: this.getValueDescription(value)}
-    //        } else {
-    //          return {}
-    //        }
-    //      } else {
-    //        // Map, list, or text objects
-    //        const conflicts = object[CONFLICTS][key], values = {}
-    //        if (!conflicts) {
-    //          throw new RangeError(`No children at key ${key} of path ${JSON.stringify(path)}`)
-    //        }
-    //        for (let opId of Object.keys(conflicts)) {
-    //          values[opId] = this.getValueDescription(conflicts[opId])
-    //        }
-    //        return values
-    //      }
-    //    }
 
     /**
      * Updates the list object at path `path`, replacing the current value at
@@ -600,29 +455,6 @@ final class Context {
         }
 
     }
-    
-    //    setListIndex(path, index, value) {
-    //      const objectId = path.length === 0 ? ROOT_ID : path[path.length - 1].objectId
-    //      const list = this.getObject(objectId)
-    //      if (index === list.length) {
-    //        return this.splice(path, index, 0, [value])
-    //      }
-    //      if (index < 0 || index > list.length) {
-    //        throw new RangeError(`List index ${index} is out of bounds for list of length ${list.length}`)
-    //      }
-    //      if (list[index] instanceof Counter) {
-    //        throw new RangeError('Cannot overwrite a Counter object; use .increment() or .decrement() to change its value.')
-    //      }
-    //
-    //      // If the assigned list element value is the same as the existing value, and
-    //      // the assignment does not resolve a conflict, do nothing
-    //      if (list[index] !== value || Object.keys(list[CONFLICTS][index] || {}).length > 1 || value === undefined) {
-    //        this.applyAtPath(path, subpatch => {
-    //          const valuePatch = this.setValue(objectId, index, value, false)
-    //          subpatch.props[index] = {[this.actorId]: valuePatch}
-    //        })
-    //      }
-    //    }
 
     /**
      * Updates the table object at path `path`, adding a new entry `row`.
@@ -640,25 +472,6 @@ final class Context {
         return valuePatch.objectId!
     }
 
-
-    //    addTableRow(path, row) {
-    //      if (!isObject(row) || Array.isArray(row)) {
-    //        throw new TypeError('A table row must be an object')
-    //      }
-    //      if (row[OBJECT_ID]) {
-    //        throw new TypeError('Cannot reuse an existing object as table row')
-    //      }
-    //      if (row.id) {
-    //        throw new TypeError('A table row must not have an "id" property; it is generated automatically')
-    //      }
-    //
-    //      const valuePatch = this.setValue(path[path.length - 1].objectId, null, row, false)
-    //      this.applyAtPath(path, subpatch => {
-    //        subpatch.props[valuePatch.objectId] = {[valuePatch.objectId]: valuePatch}
-    //      })
-    //      return valuePatch.objectId
-    //    }
-
     /**
      * Updates the table object at path `path`, deleting the row with ID `rowId`.
      */
@@ -675,16 +488,6 @@ final class Context {
             })
         }
     }
-    //    deleteTableRow(path, rowId) {
-    //      const objectId = path[path.length - 1].objectId, table = this.getObject(objectId)
-    //
-    //      if (table.byId(rowId)) {
-    //        this.addOp({action: 'del', obj: objectId, key: rowId})
-    //        this.applyAtPath(path, subpatch => {
-    //          subpatch.props[rowId] = {}
-    //        })
-    //      }
-    //    }
 
     /**
      * Adds the integer `delta` to the value of the counter located at property
@@ -718,19 +521,5 @@ final class Context {
             subpatch.props?[key] = [actorId.actorId: .value(.init(value: .int(counterValue + delta), datatype: .counter))]
         })
     }
-    //    increment(path, key, delta) {
-    //      const objectId = path.length === 0 ? ROOT_ID : path[path.length - 1].objectId
-    //      const object = this.getObject(objectId)
-    //      if (!(object[key] instanceof Counter)) {
-    //        throw new TypeError('Only counter values can be incremented')
-    //      }
-    //
-    //      // TODO what if there is a conflicting value on the same key as the counter?
-    //      const value = object[key].value + delta
-    //      this.addOp({action: 'inc', obj: objectId, key, value: delta})
-    //      this.applyAtPath(path, subpatch => {
-    //        subpatch.props[key] = {[this.actorId]: {value, datatype: 'counter'}}
-    //      })
-    //    }
 
 }
